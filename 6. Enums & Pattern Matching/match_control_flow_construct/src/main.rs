0711d52efdb0dade83b53088c997154b4a06d80b
Aperezvigoa <108186387+Aperezvigoa@users.noreply.github.com>
2025-06-06 12:45:33 +0200

    Binding enums values with match

--- 6. Enums & Pattern Matching/match_control_flow_construct/src/main.rs
+++ 6. Enums & Pattern Matching/match_control_flow_construct/src/main.rs
@@ -5,12 +5,30 @@
 // The power of match comes from the expressiveness of the pattern and the fact that the compiler
 // confirms that all posible cases are handled.
 
+// Patterns that Bind to Values
+// A useful feature of march arms is that they can bind to the parts of the values that match the
+// pattern.
+// As an example, let's change one of our enum variants to hold data inside it. From 1999 though
+// 2008, USA minted quarters with different design for each of the 50 states in ine side.
+// No other coins got state designs, so only quarters have this extra value. We can add this info
+// to out enum by changing quarter variant to include a UsState value stored.
+
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
 }
+
+// Now in the match expression for this code, we add a variable called state to the pattern that
+// matches values of the variant. When a Coin::Quarter matches, the state variable will bind to the
+// value of that quarter's state.
 
 fn value_in_cents(coin: Coin) -> u8 {
     match coin {
@@ -20,8 +38,13 @@ fn value_in_cents(coin: Coin) -> u8 {
         }
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("State quarter from {state:?}");
+            25
+        }
     }
 }
 
-fn main() {}
+fn main() {
+    value_in_cents(Coin::Quarter(UsState::Alaska));
+}
